7fe930bcf4c8dc4ad147dc47f7277a27ba8b2542
Rafael Lopez <lpz.raf@gmail.com>
2018-07-08 18:18:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   
   def name= (dogs_name)
-    @this_dogs_name = dogs_name
+    @this_dogs_name = fido
   end
   
   def name
@@ -10,7 +10,7 @@ class Dog
 end
 
 
-campeon = Dog.new
-campeon.name = "Campeon"
+fido = Dog.new
+fido.name = "Fido"
  
-puts campeon.name
+puts fido.name
